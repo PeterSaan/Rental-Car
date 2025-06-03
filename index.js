17be7e812b0cb137123b23b11a064f2fdcc38623
PeterSaan <saanpeter@gmail.com>
2025-06-03 22:03:08 +0300

    Update, change to ES5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const rental = require('./rentalPrice');
-const fs = require('fs');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { price } from './rentalPrice.js';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
@@ -14,29 +14,29 @@ app.use('/pictures', express.static('images'));
 const formHtml = fs.readFileSync('form.html', 'utf8');
 
 app.post('/', (req, res) => {
-    const post = req.body;
-		let resultHtml = fs.readFileSync('result.html', 'utf8');
-		const carTypes = ["Compact", "Electric", "Cabrio", "Racer"];
+	const post = req.body;
+	let resultHtml = fs.readFileSync('result.html', 'utf8');
+	const carTypes = ["Compact", "Electric", "Cabrio", "Racer"];
 
-		carTypes.forEach((type) => {
-			let result = rental.price(
-				Date.parse(post.licensestart),
-				Date.parse(post.pickupdate),
-				Date.parse(post.dropoffdate),
-				String(type),
-				Number(post.age)
-			);
+	carTypes.forEach((type) => {
+		let result = price(
+			Date.parse(post.licensestart),
+			Date.parse(post.pickupdate),
+			Date.parse(post.dropoffdate),
+			String(type),
+			Number(post.age)
+		);
 		resultHtml = resultHtml.replace(`${type}$0`, result);
-		})
-    
-    res.send(formHtml + resultHtml);
+	})
+
+	res.send(formHtml + resultHtml);
 });
 
 app.get('/', (req, res) => {
-    res.send(formHtml);
+	res.send(formHtml);
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+	console.log(`Server listening at http://localhost:${port}`);
 });
--- rentalPrice.js
+++ rentalPrice.js
@@ -1,34 +1,34 @@
 function getSeason(pickupDate, dropoffDate) {
-  const pickup = new Date(pickupDate);
-  const dropoff = new Date(dropoffDate);
+	const pickup = new Date(pickupDate);
+	const dropoff = new Date(dropoffDate);
 
-  const start = 4; 
-  const end = 10;
+	const start = 4; 
+	const end = 10;
 
-  const pickupMonth = pickup.getMonth();
-  const dropoffMonth = dropoff.getMonth();
+	const pickupMonth = pickup.getMonth();
+	const dropoffMonth = dropoff.getMonth();
 
-  if (
-      (pickupMonth >= start && pickupMonth <= end) ||
-      (dropoffMonth >= start && dropoffMonth <= end) ||
-      (pickupMonth < start && dropoffMonth > end)
-  ) {
-      return "High";
-  } else {
-      return "Low";
-  }
+	if (
+		(pickupMonth >= start && pickupMonth <= end) ||
+			(dropoffMonth >= start && dropoffMonth <= end) ||
+			(pickupMonth < start && dropoffMonth > end)
+	) {
+		return "High";
+	} else {
+		return "Low";
+	}
 }
 
 function getDays(pickupDate, dropoffDate) {
-  const dayMs = 24 * 60 * 60 * 1000;
-  const firstDate = new Date(pickupDate);
-  const secondDate = new Date(dropoffDate);
+	const dayMs = 24 * 60 * 60 * 1000;
+	const firstDate = new Date(pickupDate);
+	const secondDate = new Date(dropoffDate);
 
 	if (firstDate.getTime() > secondDate.getTime() || firstDate.getTime() < new Date().setHours(1, 59, 0) ) {
 		return null;
 	}
 
-  return Math.round(Math.abs((firstDate - secondDate) / dayMs)) + 1;
+	return Math.round(Math.abs((firstDate - secondDate) / dayMs)) + 1;
 }
 
 function getLicenseYears(startDate) {
@@ -45,14 +45,14 @@ function getLicenseYears(startDate) {
 	return yearsBetween.toFixed(0);
 }
 
-function price(licenseStart, pickupDate, dropoffDate, type, age) {
-  const days = getDays(pickupDate, dropoffDate);
-  const season = getSeason(pickupDate, dropoffDate);
+export function price(licenseStart, pickupDate, dropoffDate, type, age) {
+	const days = getDays(pickupDate, dropoffDate);
+	const season = getSeason(pickupDate, dropoffDate);
 	const licenseYears = getLicenseYears(licenseStart)
 
-  if (age < 18) {
-      return "Driver too young - cannot quote the price";
-  }
+	if (age < 18) {
+		return "Driver too young - cannot quote the price";
+	}
 
 	if (licenseYears < 1) {
 		return "Unable to rent because of driving license";
@@ -62,11 +62,11 @@ function price(licenseStart, pickupDate, dropoffDate, type, age) {
 		return "Invalid date"
 	}
 
-  if (age <= 21 && type !== "Compact") {
-      return "Drivers 21 y/o or less can only rent Compact vehicles";
-  }
+	if (age <= 21 && type !== "Compact") {
+		return "Drivers 21 y/o or less can only rent Compact vehicles";
+	}
 
-  let rentalprice = age;
+	let rentalprice = age;
 
 	if (licenseYears < 3 && season === "High") {
 		rentalprice += 15;
@@ -80,15 +80,13 @@ function price(licenseStart, pickupDate, dropoffDate, type, age) {
 		rentalprice *= 1.5;
 	}
 
-  if (season === "High" ) {
-    rentalprice *= 1.15;
-  }
+	if (season === "High" ) {
+		rentalprice *= 1.15;
+	}
 
-  if (days > 10 && season === "Low" ) {
-      rentalprice *= 0.9;
-  }
+	if (days > 10 && season === "Low" ) {
+		rentalprice *= 0.9;
+	}
 
-  return `$${rentalprice.toFixed(2)}`;
+	return `$${rentalprice.toFixed(2)}`;
 }
-
-exports.price = price;
